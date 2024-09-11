05c9bb9e8f6a0ba2dfa30b79ff466af5922b9032
Bill Gallagher <wgallagher86@gmail.com>
2024-09-11 11:30:46 -0700

    Adds functionality to get user by email from DB

--- crud.py
+++ crud.py
@@ -3,6 +3,11 @@ from sqlalchemy.orm import Session
 from . import models, schemas
 
 
+def get_user_by_email(db: Session, email: str):
+    return db.query(models.UserDatabase).filter(
+            models.UserDatabase.email == email).first()
+
+
 def create_user(db: Session, user: schemas.UserCreate):
     uuid = schemas.generateUUID()
     hashed_password = schemas.get_password_hash(user.password)
@@ -15,3 +20,5 @@ def create_user(db: Session, user: schemas.UserCreate):
     db.commit()
     db.refresh(db_user)
     return db_user
+
+
