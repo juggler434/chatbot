9e07418acd8959cb35e9ee854c472fae71e1bdc0
Bill Gallagher <wgallagher86@gmail.com>
2024-09-11 13:11:16 -0700

    Adds model for message

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.orm import relationship
 
 from .database import Base
@@ -9,3 +9,13 @@ class User(Base):
     id = Column(String, primary_key=True)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
+
+
+class Message(Base):
+    __tablename__ = "messages"
+    id = Column(String, primary_key=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    question = Column(String)
+    response = Column(String)
+    createdAt = Column(DateTime)
+    updatedAt = Column(DateTime)
