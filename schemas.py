343a8988f77c909b57d714b781057f4a66f975be
Bill Gallagher <wgallagher86@gmail.com>
2024-09-11 12:50:33 -0700

    Login Endpoint (#16)

--- schemas.py
+++ schemas.py
@@ -1,6 +1,14 @@
 from pydantic import BaseModel, EmailStr
 from passlib.context import CryptContext
 import uuid
+from datetime import datetime, timedelta, timezone
+import jwt
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 
 class User(BaseModel):
@@ -16,6 +24,15 @@ class UserDatabase(User):
     hashed_password: str
 
 
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    email: str | None = None
+
+
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
@@ -25,3 +42,21 @@ def get_password_hash(password):
 
 def generateUUID():
     return str(uuid.uuid4())
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.now(timezone.utc) + access_token_expires
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode,
+                             os.getenv("AUTH_SECRET_KEY"),
+                             algorithm=os.getenv("AUTH_ALGORITHM"))
+    print("does this print anything?")
+    return encoded_jwt
+
+
