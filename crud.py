af7b64601baf958a7d227f7268600c6bf85f70bf
Bill Gallagher <wgallagher86@gmail.com>
2024-09-12 13:00:22 -0700

    Creates endpoint to edit a message (#19)

--- crud.py
+++ crud.py
@@ -2,6 +2,7 @@ from sqlalchemy.orm import Session
 
 from . import models, schemas
 from datetime import datetime
+import uuid
 
 
 def get_user_by_email(db: Session, email: str):
@@ -42,3 +43,17 @@ def create_message(db: Session, message: schemas.MessageCreate):
 def get_user_messages(db: Session, user_id: str, offset: int, limit: int):
     return db.query(models.Message).filter(models.Message.user_id == user_id).order_by(models.Message.created_at.desc()).offset(offset).limit(limit).all()
 
+
+def update_message(db: Session,
+                   message_id: str,
+                   user_id: str,
+                   updated_question: str):
+    message = db.query(models.Message).filter(
+            models.Message.id == message_id,
+            models.Message.user_id == user_id).first()
+    message.question = updated_question
+    message.updated_at = datetime.now()
+    db.add(message)
+    db.commit()
+    db.refresh(message)
+    return message
