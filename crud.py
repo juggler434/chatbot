f0ef42025eada1fb30f69eb372df5cdb7d069569
Bill Gallagher <wgallagher86@gmail.com>
2024-09-12 09:58:30 -0700

    adds method to get all messages belonging to a user

--- crud.py
+++ crud.py
@@ -37,3 +37,11 @@ def create_message(db: Session, message: schemas.MessageCreate):
     db.commit()
     db.refresh(db_message)
     return db_message
+
+
+def get_user_messages(db: Session, user_id: str, offset: int, limit: int):
+    return db.query(models.Item) \
+        .filter(models.Item.user_id == user_id) \
+        .order_by(models.Message.created_at.desc()) \
+        .offset(offset) \
+        .limit(limit)
